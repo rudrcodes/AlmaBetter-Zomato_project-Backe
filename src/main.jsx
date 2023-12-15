64eaf80310d50bba4040840e44f27d434bd09d12
Gaurav verma <94301843+abcdeCoder@users.noreply.github.com>
2023-12-15 17:16:53 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import axios from 'axios'
-axios.defaults.baseURL = "http://localhost:8080/"
-console.log("ENV HOST LINK : ", axios.defaults.baseURL)
+//axios.defaults.baseURL = "http://localhost:8080/"
+//console.log("ENV HOST LINK : ", axios.defaults.baseURL)
+// eslint-disable-next-line no-unused-vars
+import dotenv from "dotenv"
+console.log("ENV HOST LINK : ", import.meta.env.VITE_AXIOS_BASEURL)
+axios.defaults.baseURL = import.meta.env.VITE_AXIOS_BASEURL
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
